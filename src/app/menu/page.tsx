a3309ae4f99a93bd02337765a2b02a4445a88f7b
snow6692 <ahmedha258258@gmail.com>
2025-01-23 17:57:48 +0200

    demo menu with categories

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,10 +1,21 @@
+import Menu from "@/components/menu/Menu";
+import { getProductsByCategory } from "@/server/db/products";
 import React from "react";
 
-function page() {
+async function page() {
+  const categories = await getProductsByCategory();
+  console.log(categories);
   return (
-    <section>
-      menu
-    </section>
+    <main>
+      {categories.map((category) => (
+        <section key={category.id} className="section-gap">
+          <div className="container text-center">
+            <h1 className="mb-6 text-4xl font-bold">{category.name}</h1>
+            <Menu items={category.products} />
+          </div>
+        </section>
+      ))}
+    </main>
   );
 }
 
--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,6 +1,24 @@
 import { cache } from "@/lib/cache";
 import prisma from "@/lib/prisma";
 
+export const getProductsByCategory = cache(
+  () => {
+    const products = prisma.category.findMany({
+      include: {
+        products: {
+          include: {
+            sizes: true,
+            extras: true,
+          },
+        },
+      },
+    });
+    return products;
+  },
+  ["products-by-category"],
+  { revalidate: 3600 },
+);
+
 export const getBestSellers = cache(
   (limit?: number | undefined) => {
     const bestSellers = prisma.product.findMany({
